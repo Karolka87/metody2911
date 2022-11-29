89d9cbe47c066a3552f828749eb4832880f68652
Karolka87 <115976221+Karolka87@users.noreply.github.com>
2022-11-29 23:26:32 +0100

    update1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,16 @@ public class Main {
     public static void main(String[] args) {
 
         Test first = new Test();
-       first.wypisz();
+        first.wypisz();
+
+        int wynikDodawania;
+        wynikDodawania = first.dodaj(44, 431);
+        System.out.println(wynikDodawania);
+
+        double wynikDzielenia = first.dzielenie(89, 0);
+        System.out.println(wynikDzielenia);
+
+
 
     }
 }
@@ -11,5 +20,14 @@ public class Main {
 
     void wypisz() {
         System.out.println("Jestem metoda testowa");
+    }
+    int dodaj(int a, int b) {
+        return a + b;
+    }
+    double dzielenie(double x, double y){
+        if (y == 0){
+            return 0;
+        }
+        return x / y;
     }
     }
