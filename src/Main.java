6a3a64d674581620d83bad184b3b4db60c4bc111
Karolka87 <115976221+Karolka87@users.noreply.github.com>
2022-11-29 23:08:07 +0100

    update1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Test first = new Test();
+       first.wypisz();
+    }
+}
+
+    class Test {
+
+    void wypisz() {
+        System.out.println("Jestem metoda testowa");
+    }
+    }
